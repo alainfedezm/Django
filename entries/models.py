89430fd40506a8673fd956cb9e043dea57f890a6
Alain Fernandez <alainfedez@gmail.com>
2022-05-25 15:03:20 +0200

    Models Migration 01

--- entries/models.py
+++ entries/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class EntryModel(models.Model):
+class Entry(models.Model):
     datetime = models.DateTimeField()
     concept = models.CharField(max_length=255)
     amount = models.FloatField()
