8338fca2b8ab840aa7de77c5c691812473ea43d4
Alain Fernandez <alainfedez@gmail.com>
2022-05-25 14:01:59 +0200

    Models Migration 01

--- api/settings.py
+++ api/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest-framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'entries',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
